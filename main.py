e83dc6dd8a468eb3f457ddcffbae3ad1eac46137
t-asai <t-asai@inbility.co.jp>
2021-10-12 17:59:56 +0900

    コマンドライン引数の修正

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ parser.add_argument('--show', help='表示フラグ', action='store_true')
 
 parser.add_argument('--out_dir', help='出力ディレクトリ', default='./')
 parser.add_argument('--out_movie', help='動画ファイルパス')
-parser.add_argument('--out_file', help='分割ファイル場所', type=bool)
+parser.add_argument('--out_file', help='分割ファイル場所', action='store_true')
 parser.add_argument('--out_size', help='動画サイズ')
 parser.add_argument('--fps', help='フレームレート', default=None, type=int)
 parser.add_argument('--movie_extension', help='動画ファイル拡張子', default='mp4')
@@ -48,6 +48,7 @@ def common_process(image, video_writer, fps, size):
     if args.out_movie is not None:
         if video_writer is None:
             video_writer = make_video_writer(fps, size)
+            video_writer.write(image)
         else:
             video_writer.write(image)
 
